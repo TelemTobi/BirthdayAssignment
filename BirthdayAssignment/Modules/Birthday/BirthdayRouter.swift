dac3c345fa9f16e683da7fc5fcad1f1b03a5a28b
Telem Tobi <51072238+TelemTobi@users.noreply.github.com>
2024-01-28 17:38:37 +0200

    Create Birthday module

---
+++ BirthdayAssignment/Modules/Birthday/BirthdayRouter.swift
@@ -0,0 +1,12 @@
+//
+//  BirthdayRouter.swift
+//  BirthdayAssignment
+//
+//  Created by Telem Tobi on 28/01/2024.
+//
+
+import Foundation
+
+protocol BirthdayRouter where Self : Coordinator {
+    
+}
---
+++ BirthdayAssignment/Modules/Birthday/BirthdayViewModel.swift
@@ -0,0 +1,50 @@
+//
+//  BirthdayViewModel.swift
+//  BirthdayAssignment
+//
+//  Created by Telem Tobi on 28/01/2024.
+//
+
+import SwiftUI
+
+class BirthdayViewModel: ViewModel, ObservableObject {
+    
+    @Published private(set) var state: State
+    private weak var router: BirthdayRouter?
+    
+    init(state: State, router: BirthdayRouter?) {
+        self.state = state
+        self.router = router
+    }
+    
+    struct State: Equatable, Hashable {
+        var baby: Baby
+        
+        var title: LocalizedStringKey {
+            "TODAY \(baby.name.uppercased()) IS"
+        }
+        
+        var age: Int {
+            let monthsAge = baby.birthdate.monthsPassed
+            return monthsAge < 12 ? monthsAge : monthsAge / 12
+        }
+        
+        var subtitle: LocalizedStringKey {
+            let monthsAge = baby.birthdate.monthsPassed
+            return if monthsAge < 12 { monthsAge == 1 ? "MONTH OLD" : "MONTHS OLD" }
+            else { (monthsAge / 12) == 1 ? "YEAR OLD" : "YEARS OLD" }
+        }
+    }
+    
+    enum Action {
+        case setPicute(Data?)
+    }
+    
+    func send(_ action: Action) {
+        switch action {
+        case let .setPicute(data):
+            state.baby.picture = data
+            UserDefaults.standard.baby = state.baby
+        }
+    }
+}
